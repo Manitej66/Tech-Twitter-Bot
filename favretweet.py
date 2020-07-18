8cbdde5d052ee2e5e2008abcdfe62130b01aa868
virus2475 <pratamaniteja66@gmail.com>
2020-07-18 15:25:08 +0530

    update

--- favretweet.py
+++ favretweet.py
@@ -1,46 +1,63 @@
+# import tweepy
+# import logging
+# from config import create_api
+# import json
+
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger()
+
+# class FavRetweetListener(tweepy.StreamListener):
+#     def __init__(self, api):
+#         self.api = api
+#         self.me = api.me()
+
+#     def on_status(self, tweet):
+#         logger.info("Processing tweet id" + str(tweet.id))
+#         if tweet.in_reply_to_status_id is not None or \
+#             tweet.user.id == self.me.id:
+#             # This tweet is a reply or I'm its author so, ignore it
+#             return
+#         if not tweet.favorited:
+#             # Mark it as Liked, since we have not done it yet
+#             try:
+#                 tweet.favorite()
+#             except Exception as e:
+#                 logger.error("Error on fav", exc_info=True)
+#         if not tweet.retweeted:
+#             # Retweet, since we have not retweeted it yet
+#             try:
+#                 tweet.retweet()
+#             except Exception as e:
+#                 logger.error("Error on fav and retweet", exc_info=True)
+
+#     def on_error(self, status):
+#         logger.error(status)
+
+# def main(keywords):
+#     api = create_api()
+#     tweets_listener = FavRetweetListener(api)
+#     stream = tweepy.Stream(api.auth, tweets_listener)
+#     stream.filter(track=keywords, languages=["en"])
+
+# if __name__ == "__main__":
+#     main(["Tech", "Technews"])
+import os
+
 import tweepy
-import logging
-from config import create_api
-import json
+from time import sleep
+auth = tweepy.OAuthHandler(os.getenv("CONSUMER_KEY"),
+                           os.getenv("CONSUMER_SECRET"))
+auth.set_access_token(os.getenv("ACCESS_KEY"), os.getenv("ACCESS_SECRET"))
+api = tweepy.API(auth)
+for tweet in tweepy.Cursor(api.search, q='#tech').items(5):
+    try:
+        print('\nRetweet Bot found tweet by @' + tweet.user.screen_name +
+              '. ' + 'Attempting to retweet.')
+        tweet.retweet()
+        print('Retweet published successfully.')
+        sleep(10)
+    except tweepy.TweepError as error:
+        print(error.reason)
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger()
-
-
-class FavRetweetListener(tweepy.StreamListener):
-    def __init__(self, api):
-        self.api = api
-        self.me = api.me()
-
-    def on_status(self, tweet):
-        logger.info("Processing tweet id" + str(tweet.id))
-        if tweet.in_reply_to_status_id is not None or \
-            tweet.user.id == self.me.id:
-            # This tweet is a reply or I'm its author so, ignore it
-            return
-        if not tweet.favorited:
-            # Mark it as Liked, since we have not done it yet
-            try:
-                tweet.favorite()
-            except Exception as e:
-                logger.error("Error on fav", exc_info=True)
-        if not tweet.retweeted:
-            # Retweet, since we have not retweeted it yet
-            try:
-                tweet.retweet()
-            except Exception as e:
-                logger.error("Error on fav and retweet", exc_info=True)
-
-    def on_error(self, status):
-        logger.error(status)
-
-
-def main(keywords):
-    api = create_api()
-    tweets_listener = FavRetweetListener(api)
-    stream = tweepy.Stream(api.auth, tweets_listener)
-    stream.filter(track=keywords, languages=["en"])
-
-
-if __name__ == "__main__":
-    main(["Tech", "Technews"])
+    except StopIteration:
+        break
